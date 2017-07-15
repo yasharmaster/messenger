1374e55dc6d19c881a929ba4558f573f07f2c674
Yash Sharma <yasharmaster@gmail.com>
2017-07-15 18:29:03 +0530

    Create new message using POST request

--- src/main/java/org/yash/messenger/resources/MessageResource.java
+++ src/main/java/org/yash/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package org.yash.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,11 +24,17 @@ public class MessageResource {
 		return messageService.getAllMessages();
 	}
    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON) 
+    public Message addMessage(Message message) {
+    	return messageService.addMessage(message);
+    }
     
     @GET
     @Path("/{messageId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Message test(@PathParam("messageId") long messageId) {
+    public Message getMessage(@PathParam("messageId") long messageId) {
     	return messageService.getMessage(messageId);
     }
 }
